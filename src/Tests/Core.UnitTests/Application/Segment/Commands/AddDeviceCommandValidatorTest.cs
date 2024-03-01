b70779ab272af2c5d717ce9860a12985575dfda5
Mohammad Mokhtari <Mohammad.Mokhtari86@gmail.com>
2024-03-01 11:53:33 +0330

    add more unittests for AddDeviceCommandValidator

--- src/Tests/Core.UnitTests/Application/Segment/Commands/AddDeviceCommandValidatorTest.cs
+++ src/Tests/Core.UnitTests/Application/Segment/Commands/AddDeviceCommandValidatorTest.cs
@@ -90,6 +90,53 @@ public class AddDeviceCommandValidatorTest
         result.Errors.Any(x => x.PropertyName == nameof(command.SubscriberId) && x.ErrorCode == nameof(AppResource.NotFound)).Should().BeTrue();
     }
 
+
+    [Fact]
+    public async Task Validate_Should_ReturnValidationError_When_PushManagerIsInvalid()
+    {
+        //Arrange
+        string deviceName = "FakeName";
+        PushManager devicePushManager = PushManager.Create("", "", "");
+        ClientMetadata deviceClientMetadata = ClientMetadata.Create("Android");
+        int subscriberId = 12;
+
+        AddDeviceCommand command = new(deviceName, devicePushManager, deviceClientMetadata, subscriberId);
+
+        _subscriberRepository.ExistsAsync(subscriberId, default).Returns(false);
+
+        //Act
+        var result = await _validator.ValidateAsync(command, default);
+
+        //Assert
+        result.IsValid.Should().BeFalse();
+        result.Errors.Any(x => x.PropertyName == $"{nameof(command.PushManager)}.{nameof(command.PushManager.Endpoint)}" && x.ErrorCode == nameof(AppResource.NotEmpty)).Should().BeTrue();
+        result.Errors.Any(x => x.PropertyName == $"{nameof(command.PushManager)}.{nameof(command.PushManager.P256DH)}" && x.ErrorCode == nameof(AppResource.NotEmpty)).Should().BeTrue();
+        result.Errors.Any(x => x.PropertyName == $"{nameof(command.PushManager)}.{nameof(command.PushManager.Auth)}" && x.ErrorCode == nameof(AppResource.NotEmpty)).Should().BeTrue();
+    }
+
+
+    [Fact]
+    public async Task Validate_Should_ReturnValidationError_When_ClientMetadataIsInvalid()
+    {
+        //Arrange
+        string deviceName = "FakeName";
+        PushManager devicePushManager = PushManager.Create("Endpoint", "P256", "Auth");
+        ClientMetadata deviceClientMetadata = ClientMetadata.Create("");
+        int subscriberId = 12;
+
+        AddDeviceCommand command = new(deviceName, devicePushManager, deviceClientMetadata, subscriberId);
+
+        _subscriberRepository.ExistsAsync(subscriberId, default).Returns(false);
+
+        //Act
+        var result = await _validator.ValidateAsync(command, default);
+
+        //Assert
+        result.IsValid.Should().BeFalse();
+        result.Errors.Any(x => x.PropertyName == $"{nameof(command.ClientMetadata)}.{nameof(command.ClientMetadata.OS)}" && x.ErrorCode == nameof(AppResource.NotEmpty)).Should().BeTrue();
+    }
+
+
     [Fact]
     public async Task Validate_Should_ReturnSuccess_When_InputIsValid()
     {
@@ -102,6 +149,7 @@ public class AddDeviceCommandValidatorTest
         AddDeviceCommand command = new(deviceName, devicePushManager, deviceClientMetadata, subscriberId);
 
         _subscriberRepository.ExistsAsync(subscriberId, default).Returns(true);
+
         //Act
         var result = await _validator.ValidateAsync(command, default);
 
