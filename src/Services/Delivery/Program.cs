bdcb1cc78d1c76577a0966ee344cceb5b224b401
sobi <sobhanmozafari1995@gmail.com>
2024-03-09 16:59:16 +0330

    add rabit

--- src/Services/Delivery/Program.cs
+++ src/Services/Delivery/Program.cs
@@ -1,7 +1,27 @@
 using Delivery;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
 
 var builder = Host.CreateApplicationBuilder(args);
 builder.Services.AddHostedService<Worker>();
 
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+var consumer = new EventingBasicConsumer(channel);
+consumer.Received += (model, eventArgs) =>
+{
+    var body = eventArgs.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine(message);
+};
+
+
+
 var host = builder.Build();
 host.Run();
+
+
